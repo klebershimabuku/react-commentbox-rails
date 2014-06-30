aff0b0bdcae838c4c992e91d010f8d0e70a7f4c9
Kleber Shimabuku <klebershimabuku@gmail.com>
2014-06-30 00:34:12 -0300

    Creating new comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,4 +4,9 @@ class CommentsController < ApplicationController
   def index
     respond_with(Comment.all)
   end
+
+  def create
+    comment = Comment.create(author: params["author"], comment: params["comment"])
+    respond_with(comment)
+  end
 end
